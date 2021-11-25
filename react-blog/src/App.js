4202d24eb93b9b40568fbc5175b5f9fc4d80fefd
sizuki159 <luahantinhthu9@gmail.com>
2021-11-25 17:14:47 +0700

    header

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,9 +1,12 @@
+import Header from "./header/Header";
+import Home from "./pages/home/Home";
 import TopBar from "./topbar/TopBar";
 
 function App() {
   return (
     <>
       <TopBar></TopBar>
+      <Home></Home>
     </>
   );
 }
