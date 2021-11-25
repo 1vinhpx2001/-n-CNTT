95a5d711aba6bbf7a645d1c2f6ef270e5dd9518b
sizuki159 <luahantinhthu9@gmail.com>
2021-11-25 19:01:57 +0700

    Final

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,15 +1,28 @@
-import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import user from "./components/topbar/TopBar";
+import Homepage from "./pages/homepage/Homepage";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <TopBar />
-      <Settings />
-    </>
+      <Routes>
+        <Route exact path="/" element = {<Homepage/>}/>
+        <Route path="/posts" element = {<Homepage/>}/>
+        <Route path="/register"  element = {<Register />}/>
+        <Route path="/login" element= {user ? <Login /> : <Homepage/>}/>
+        <Route path="/post/:id" element ={<Single/>} />
+        <Route path="/write" element ={user ? <Write /> : <Login/>}/>
+        <Route path="/settings" element = {user ? <Settings /> : <Login/>}/>
+      </Routes>
+    </Router>
   );
 }
 
